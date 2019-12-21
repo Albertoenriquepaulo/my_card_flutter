8f199e9ab72c622ce70e375cb75e6b7ff7baa7b6
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2019-12-21 03:11:48 +0100

    First Commit from Android Studio (AS)

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,138 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp(),
   );
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.white,
+              backgroundImage: AssetImage('images/pic_wo_background.png'),
+            ),
+            Text(
+              'Alberto E Paulo',
+              style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'FUTURE FLUTTER DEVELOPER',
+              style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 5),
+              child: SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                  thickness: 1,
+                ),
+              ),
+            ),
+            Card(
+                margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+34 633824409',
+                    style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      color: Colors.teal.shade900,
+                      fontSize: 20.0,
+//                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                )),
+            Card(
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'Albertopaulo@gmail.com',
+                    style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      color: Colors.teal.shade900,
+                      fontSize: 20.0,
+//                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ))
+          ],
+        )),
+      ),
+    );
+  }
+}
+
+//// Section 6. Video 40. Flutter Layouts Challenge
+//class MyApp extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      home: Scaffold(
+//        backgroundColor: Colors.teal,
+//        body: SafeArea(
+//            child: Row(
+//          crossAxisAlignment: CrossAxisAlignment.stretch,
+//          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//          children: <Widget>[
+//            Container(
+//              width: 100,
+//              color: Colors.yellow,
+//            ),
+////            SizedBox(width: 20),
+//            Container(
+//              width: 100,
+////              color: Colors.blue,
+//              child: Column(
+//                mainAxisAlignment: MainAxisAlignment.center,
+//                children: <Widget>[
+//                  Container(
+//                    height: 100,
+//                    width: 100,
+//                    color: Colors.blue,
+//                  ),
+//                  Container(
+//                    height: 100,
+//                    width: 100,
+//                    color: Colors.blueGrey,
+//                  ),
+//                ],
+//              ),
+//            ),
+////            SizedBox(width: 20),
+//            Container(
+//              width: 100,
+//              color: Colors.red,
+//            ),
+//          ],
+//        )),
+//      ),
+//    );
+//  }
+//}
